e4136acd24b304cda7da46574365283e0bb712cb
luisruarios <luis.rua@guarapo.dev>
2022-03-01 16:53:35 -0500

    create-connection-and-fetch-data

---
+++ back-end/data/users.js
@@ -0,0 +1,20 @@
+import bcrypt from "bcryptjs/dist/bcrypt"
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashsyn
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: xxxx,
+    },
+    {
+        name: 'Jane Doe',
+        email: 'Jane@example.com',
+        password: xxxx,
+    }
+]
