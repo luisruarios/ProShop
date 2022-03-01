b13e73b74177654e1201222a52603f791f73ebd6
luisruarios <luis.rua@guarapo.dev>
2022-03-01 16:53:55 -0500

    create-connection-and-fetch-data

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose'
+import colors from 'colors'
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-
-
-        console.log(`MongoDB connected: ${conn.connection.host}`)
+        console.log(`MongoDB connected: ${conn.connection.host}`.green.bold)
     } catch (error) {
         console.error(`Error: ${error.message}`)
         process.exit(1)
--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
-connectDB
+connectDB()
 
 const app = express()
 
@@ -13,15 +14,11 @@ app.get('/', (req, res) => {
     res.send('API is running....')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
 
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
